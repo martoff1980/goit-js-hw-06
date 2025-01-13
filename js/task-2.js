9c09d338f72a2e9c57636eb5d2d88e88b37ed159
martoff <martoff@ukr.net>
2025-01-13 23:43:43 +0200

    release 13.01.2025

---
+++ js/task-2.js
@@ -0,0 +1,24 @@
+class Storage {
+  constructor(initialItems) {
+    this.items = initialItems;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.items = this.items.filter(item => item !== itemToRemove);
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems());
+storage.addItem('Droid');
+console.log(storage.getItems());
+storage.removeItem('Prolonger');
+console.log(storage.getItems());
